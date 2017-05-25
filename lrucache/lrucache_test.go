742fcc8768fbea18a481aa733727f8c54d29f00f
yowcow <yowcow@cpan.org>
2017-05-25 19:58:04 +0900

    testify/assert

--- lrucache/lrucache_test.go
+++ lrucache/lrucache_test.go
@@ -16,30 +16,26 @@ func TestNew(t *testing.T) {
 }
 
 func TestAddNode(t *testing.T) {
+	assert := assert.New(t)
+
 	cache := New(3)
 	cache.addNode(NewNode("hoge", 111))
 	cache.addNode(NewNode("fuga", 222))
 	cache.addNode(NewNode("fooo", 333))
 
-	if size := cache.CurrentSize(); size != 3 {
-		t.Error("Expected 3 but got", size)
-	}
+	assert.Equal(int64(3), cache.CurrentSize())
 
 	keys1 := cache.AllKeys()
 	keys2 := cache.AllKeysReversed()
 
-	if keys1[0] != keys2[2] {
-		t.Error("Expected equal but got", keys1[0], keys2[2])
-	}
-	if keys1[1] != keys2[1] {
-		t.Error("Expected equal but got", keys1[1], keys2[1])
-	}
-	if keys1[2] != keys2[0] {
-		t.Error("Expected equal but got", keys1[2], keys2[0])
-	}
+	assert.Equal(keys1[0], keys2[2])
+	assert.Equal(keys1[1], keys2[1])
+	assert.Equal(keys1[2], keys2[0])
 }
 
 func TestRemoveNode(t *testing.T) {
+	assert := assert.New(t)
+
 	n1 := NewNode("hoge", 111)
 	n2 := NewNode("fuga", 222)
 	n3 := NewNode("fooo", 333)
@@ -51,127 +47,87 @@ func TestRemoveNode(t *testing.T) {
 	cache.addNode(n3)
 	cache.addNode(n4)
 
-	if size := cache.CurrentSize(); size != 4 {
-		t.Error("Expected 4 but got", size)
-	}
+	assert.Equal(int64(4), cache.CurrentSize())
 
 	cache.removeNode(n2)
 
-	if size := cache.CurrentSize(); size != 3 {
-		t.Error("Expected 3 but got", size)
-	}
+	assert.Equal(int64(3), cache.CurrentSize())
 
 	keys1 := cache.AllKeys()
 	keys2 := cache.AllKeysReversed()
 
-	if keys1[0] != keys2[2] {
-		t.Error("Expected equal but got", keys1[0], keys2[2])
-	}
-	if keys1[1] != keys2[1] {
-		t.Error("Expected equal but got", keys1[1], keys2[1])
-	}
-	if keys1[2] != keys2[0] {
-		t.Error("Expected equal but got", keys1[2], keys2[0])
-	}
+	assert.Equal(keys1[0], keys2[2])
+	assert.Equal(keys1[1], keys2[1])
+	assert.Equal(keys1[2], keys2[0])
 
 	cache.removeNode(n1)
 
-	if size := cache.CurrentSize(); size != 2 {
-		t.Error("Expected 2 but got", size)
-	}
+	assert.Equal(int64(2), cache.CurrentSize())
 
 	keys1 = cache.AllKeys()
 	keys2 = cache.AllKeysReversed()
 
-	if keys1[0] != keys2[1] {
-		t.Error("Expected equal but got", keys1[0], keys2[1])
-	}
-	if keys1[1] != keys2[0] {
-		t.Error("Expected equal but got", keys1[1], keys2[0])
-	}
+	assert.Equal(keys1[0], keys2[1])
+	assert.Equal(keys1[1], keys2[0])
 
 	cache.removeNode(n4)
 
-	if size := cache.CurrentSize(); size != 1 {
-		t.Error("Expected 1 but got", size)
-	}
+	assert.Equal(int64(1), cache.CurrentSize())
 
 	keys1 = cache.AllKeys()
 	keys2 = cache.AllKeysReversed()
 
-	if keys1[0] != keys2[0] {
-		t.Error("Expected equal but got", keys1[0], keys2[0])
-	}
+	assert.Equal(keys1[0], keys2[0])
 
 	cache.removeNode(n3)
 
-	if size := cache.CurrentSize(); size != 0 {
-		t.Error("Expected 0 but got", size)
-	}
+	assert.Equal(int64(0), cache.CurrentSize())
 
 	keys1 = cache.AllKeys()
 	keys2 = cache.AllKeysReversed()
 
-	if len(keys1) != 0 {
-		t.Error("Expected no keys but got", keys1)
-	}
-	if len(keys2) != 0 {
-		t.Error("Expected no keys but got", keys2)
-	}
+	assert.Equal(0, len(keys1))
+	assert.Equal(0, len(keys2))
 }
 
 func TestSet(t *testing.T) {
+	assert := assert.New(t)
+
 	cache := New(3)
 	cache.Set("hoge", 111)
 	cache.Set("fuga", 222)
 	cache.Set("fooo", 333)
 	cache.Set("baar", 444)
 
-	if size := cache.CurrentSize(); size != 3 {
-		t.Error("Expected 3 but got", size)
-	}
+	assert.Equal(int64(3), cache.CurrentSize())
 
 	var v interface{}
 	var e error
 
 	v, e = cache.Get("hoge")
 
-	if e == nil {
-		t.Error("Expected error but got", e)
-	}
-	if v != nil {
-		t.Error("Expected nil but got", v)
-	}
+	assert.NotEqual(nil, e)
+	assert.Equal(nil, v)
 
 	v, e = cache.Get("fuga")
 
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
-	if v != 222 {
-		t.Error("Expected 222 but got", v)
-	}
+	assert.Equal(nil, e)
+	assert.Equal(222, v)
 
 	v, e = cache.Get("fooo")
 
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
-	if v != 333 {
-		t.Error("Expected 333 but got", v)
-	}
+	assert.Equal(nil, e)
+	assert.Equal(333, v)
 
 	v, e = cache.Get("baar")
 
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
-	if v != 444 {
-		t.Error("Expected 444 but got", v)
-	}
+	assert.Equal(nil, e)
+	assert.Equal(444, v)
 }
 
 func TestGet(t *testing.T) {
+	assert := assert.New(t)
+
 	cache := New(3)
 	cache.Set("hoge", 111)
 	cache.Set("fuga", 222)
@@ -182,51 +138,35 @@ func TestGet(t *testing.T) {
 
 	cache.Set("baar", 444) // "fooo" is deleted
 
-	if size := cache.CurrentSize(); size != 3 {
-		t.Error("Expected 3 but got", size)
-	}
+	assert.Equal(int64(3), cache.CurrentSize())
 
 	var v interface{}
 	var e error
 
 	v, e = cache.Get("fooo")
 
-	if v != nil {
-		t.Error("Expected nil but got", v)
-	}
-	if e == nil {
-		t.Error("Expected error but got", e)
-	}
+	assert.Equal(nil, v)
+	assert.NotEqual(nil, e)
 
 	v, e = cache.Get("hoge")
 
-	if v != 111 {
-		t.Error("Expected 111 but got", v)
-	}
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
+	assert.Equal(111, v)
+	assert.Equal(nil, e)
 
 	v, e = cache.Get("fuga")
 
-	if v != 222 {
-		t.Error("Expected 222 but got", v)
-	}
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
+	assert.Equal(222, v)
+	assert.Equal(nil, e)
 
 	v, e = cache.Get("baar")
 
-	if v != 444 {
-		t.Error("Expected 444 but got", v)
-	}
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
+	assert.Equal(444, v)
+	assert.Equal(nil, e)
 }
 
 func TestDelete(t *testing.T) {
+	assert := assert.New(t)
+
 	cache := New(3)
 	cache.Set("hoge", 111)
 	cache.Set("fuga", 222)
@@ -236,34 +176,20 @@ func TestDelete(t *testing.T) {
 
 	e = cache.Delete("fuga")
 
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
-	if s := cache.CurrentSize(); s != 2 {
-		t.Error("Expected 2 but got", s)
-	}
+	assert.Equal(nil, e)
+	assert.Equal(int64(2), cache.CurrentSize())
 
 	e = cache.Delete("fooo")
 
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
-	if s := cache.CurrentSize(); s != 1 {
-		t.Error("Expected 1 but got", s)
-	}
+	assert.Equal(nil, e)
+	assert.Equal(int64(1), cache.CurrentSize())
 
 	e = cache.Delete("hoge")
 
-	if e != nil {
-		t.Error("Expected nil but got", e)
-	}
-	if s := cache.CurrentSize(); s != 0 {
-		t.Error("Expected 0 but got", s)
-	}
+	assert.Equal(nil, e)
+	assert.Equal(int64(0), cache.CurrentSize())
 
 	e = cache.Delete("nonexisting")
 
-	if e == nil {
-		t.Error("Expected error but got", e)
-	}
+	assert.NotEqual(nil, e)
 }
